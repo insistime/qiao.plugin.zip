38607b7da8503f27228c00f34adb2c2664dd4096
uikoo9 <uikoo9@qq.com>
2017-12-28 18:00:52 +0800

    zip folder async

---
+++ test/zip-folder-async.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var qiaoPluginZip = require('../lib/qiao.plugin.zip');
+
+function test(){
+	var sourceFolder= 'd:/test/';
+	var destZip		= 'd:/test23.zip';
+	
+	qiaoPluginZip.zipFolder(sourceFolder, destZip, function(err, msg){
+		if(err) throw err;
+		
+		console.log(msg);
+	});
+};
+
+test();
