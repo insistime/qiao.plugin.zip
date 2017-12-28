d873c94a1242a069f57677ca08cb22dcf02619ed
uikoo9 <uikoo9@qq.com>
2017-12-28 17:00:40 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # qiao.plugin.zip
+---
 nodejs zip and unzip tool
+
+# install
+---
+npm install qiao.plugin.zip
+	
+# version
+---
+### 0.0.1.20171228
+1. 初始化项目
---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var qiaoPluginZip = require('../lib/qiao.plugin.zip');
+
+function test(){
+	var zipFile 	= 'd:/test1.zip';
+	var destFolder	= 'd:/test2/';
+	
+	qiaoPluginZip.unzip(zipFile, destFolder);
+};
+
+test();
